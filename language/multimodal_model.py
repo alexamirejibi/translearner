630998b279c48e8d0be77f86c41fc5076077106b
Alex Amiredjibi <alexamirejibi@gmail.com>
2022-12-16 22:33:20 +0100

    train AGAIN

--- language/multimodal_model.py
+++ language/multimodal_model.py
@@ -19,7 +19,7 @@ from multimodal_transformers.data import load_data_from_folder
 from multimodal_transformers.model import TabularConfig
 from multimodal_transformers.model import AutoModelWithTabular
 
-from utils import calc_classification_metrics
+from utils import *
 
 from dclasses import ModelArguments, MultimodalDataTrainingArguments
 
@@ -33,43 +33,26 @@ from utils import action_words
 numerical_cols = action_words
 print(numerical_cols)
 
-column_info_dict = {
-    'text_cols': text_cols,
-    'num_cols': numerical_cols,
-    #'cat_cols': cat_cols,
-    'label_col': 'label',
-    'label_list': ['0', '1', '2', '3', '4']
-}
+# column_info_dict = {
+#     'text_cols': text_cols,
+#     'num_cols': numerical_cols,
+#     #'cat_cols': cat_cols,
+#     'label_col': 'label',
+#     'label_list': ['0', '1', '2', '3', '4']
+# }
 
 
 model_args = ModelArguments(
     model_name_or_path='distilbert-base-uncased'
 )
 
-data_args = MultimodalDataTrainingArguments(
-    data_path='data/new_split/',
-    combine_feat_method='individual_mlps_on_cat_and_numerical_feats_then_concat',
-    column_info=column_info_dict,
-    task='classification',
-    categorical_encode_type='none'
-)
-
-training_args = TrainingArguments(
-    output_dir="multimodal-traj-class-no-numtransform",
-    #logging_dir="Multimodal-Trajectory-Classifier/logs/runs",
-    overwrite_output_dir=True,
-    do_train=True,
-    do_eval=True,
-    per_device_train_batch_size=32,
-    num_train_epochs=20,
-    # evaluate_during_training=True,
-    logging_steps=25,
-    eval_steps=250,
-    #use_mps_device=True, # TODO check if this is needed  <<<<<<--------------------------<<<<<<
-    push_to_hub=True,
-)
-
-set_seed(training_args.seed)
+# data_args = MultimodalDataTrainingArguments(
+#     data_path='data/new_split/',
+#     combine_feat_method='individual_mlps_on_cat_and_numerical_feats_then_concat',
+#     column_info=column_info_dict,
+#     task='classification',
+#     categorical_encode_type='none'
+# )
 
 tokenizer_path_or_name = model_args.tokenizer_name if model_args.tokenizer_name else model_args.model_name_or_path
 print('Specified tokenizer: ', tokenizer_path_or_name)
@@ -92,7 +75,6 @@ train_dataset, val_dataset, test_dataset = load_data_from_folder(
 )
 
 num_labels = len(np.unique(train_dataset.labels))
-print(num_labels)
 
 config = AutoConfig.from_pretrained('distilbert-base-uncased')
 tabular_config = TabularConfig(num_labels=num_labels,
@@ -106,6 +88,30 @@ model = AutoModelWithTabular.from_pretrained(
         config=config,
         cache_dir=model_args.cache_dir
     )
+
+
+training_args = TrainingArguments(
+    output_dir="multimodal-traj-class-no-numtransform",
+    #logging_dir="Multimodal-Trajectory-Classifier/logs/runs",
+    overwrite_output_dir=True,
+    do_train=True,
+    do_eval=True,
+    per_device_train_batch_size=32,
+    per_device_eval_batch_size=32,
+    num_train_epochs=20,
+    # evaluate_during_training=True,
+    # logging_steps=25,
+    eval_steps=250,
+    load_best_model_at_end=True,
+    metric_for_best_model='relacc',
+    save_strategy='epoch',
+    evaluation_strategy='epoch',
+    # auto_find_batch_size=True,
+    #use_mps_device=True, # TODO check if this is needed  <<<<<<--------------------------<<<<<<
+    push_to_hub=True,
+)
+
+set_seed(training_args.seed)
 
 
 trainer = Trainer(
